45fc8dabc7232a7b79038d999ddf4be4e7fe5472
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-09-03 14:27:08 -0700

    Completed MVP without using any styling.

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      myData: {},
+      myFollowers: []
+    }
+  }
+  componentDidMount() {
+    axios
+    .get('https://api.github.com/users/lcampbe79')
+    .then(res => this.setState({myData: res.data}))
+    .catch(err => console.log("Server Error"));
 
+    axios
+    .get('https://api.github.com/users/lcampbe79/followers')
+    .then(res => this.setState({myFollowers: res.data}))
+    .catch(err => console.log("Server Error"));
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <UserCard myData={this.state.myData} myFollowers={this.state.myFollowers}/>
+      </div>
+    );
+  }
+}
+function UserCard(props) {
+  return(
+    <div>
+      <h4>{props.myData.name}</h4>
+      <h3>{props.myData.login}</h3>
+      <h3>{props.myData.followers}</h3>
+      <h3>{props.myData.following}</h3>
+      <h3>{props.myData.location}</h3>
+      <div>
+        {props.myFollowers.map(follower =>
+          <div key={follower.id}>{follower.login}</div>
+          )}
+      </div>
+    </div>
+  )
+}
 export default App;
